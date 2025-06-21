2f8fd8a0d565e5ffb5718d55583cc19bb2f3d319
Khadija Hafiz <khaf0001@student.monash.edu>
2025-06-22 03:37:48 +0800

    Add dummy contact list data for testing

---
+++ src/app/data/contactList.ts
@@ -0,0 +1,28 @@
+import {Contact} from "../model/Contact";
+
+export const contacts: Contact[] = [
+    {
+        id: "01",
+        firstName: "Anna",
+        lastName: "",
+        phone: "+60123456789",
+        email: "anna123@gmail.com",
+        address: "Kuala Lumper, Malaysia",
+    },
+    {
+        id: "02",
+        firstName: "Bobby",
+        lastName: "Lee",
+        phone: "+60198765432",
+        email: "boblee313@gmail.com",
+        address: "Subang Jaya, Selangor, Malaysia",
+    },
+    {
+        id: "03",
+        firstName: "Adam",
+        lastName: "Green",
+        phone: "+60188765435",
+        email: "agr64@gmail.com",
+        address: "Petaling Jaya, Selangor, Malaysia",
+    }
+]

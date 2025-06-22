4f5653bff05c39a586e68fce31fac253b82ba8f5
Khadija Hafiz <khaf0001@student.monash.edu>
2025-06-22 18:42:30 +0800

    Add two simple buttons in home page for navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col gap-6">
+        <Link
+        href={ "addcontact" }
+        className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
+          Add a New Contact
+        </Link>
+        <Link
+        href={ "viewcontacts" }
+        className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
+          View Contacts
+        </Link>
+      </div>
+    </div>
+  );
 }

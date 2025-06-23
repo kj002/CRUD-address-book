110c817c7807b240409eb367c389ef81aa2cfa88
Khadija Hafiz <khaf0001@student.monash.edu>
2025-06-24 02:10:49 +0800

    Refactor Home component layout and include welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,32 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col gap-6">
-        <Link
-        href={ "add-contact" }
-        className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
-          Add a New Contact
-        </Link>
-        <Link
-        href={ "view-contacts" }
-        className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
-          View Contacts
-        </Link>
+    <div className="flex justify-center pt-30 h-screen">
+      <div className="flex flex-col gap-14 items-center w-full max-w-md">
+        <div className="flex flex-col items-center text-center">
+          <p className="text-4xl">
+            Welcome to 
+          </p>
+          <p className="text-6xl text-bold mt-2 mb-10">
+            PocketPal!
+          </p>
+          <p className="text-justify md:text-center">
+            PocketPal is your cozy digital address book — a simple, elegant way to save, search, and organize the people who matter most. Add favorite contacts, mark them with a star, and keep your connections just a tap away.
+          </p>
+        </div>
+        <div className="flex flex-col gap-6">
+          <Link
+          href={ "add-contact" }
+          className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
+            Add a New Contact
+          </Link>
+          <Link
+          href={ "view-contacts" }
+          className="w-64 text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
+            View Contacts
+          </Link>
+        </div>
+        
       </div>
     </div>
   );

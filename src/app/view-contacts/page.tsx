fbe8844bb284ce2aacb809f67bddaebe7aaab662
Khadija Hafiz <khaf0001@student.monash.edu>
2025-06-23 19:49:35 +0800

    Implement search functionality and sorting in ViewContactsPage

--- src/app/view-contacts/page.tsx
+++ src/app/view-contacts/page.tsx
@@ -2,36 +2,72 @@
 import ContactCard from "@/components/ContactCard";
 import { useEffect, useState } from "react";
 import { Contact } from '../../model/Contact';
+import { searchContact } from '../../lib/utils';
+import { FilterIcon, SortAscIcon, SortDescIcon } from "lucide-react";
 
-export default function ViewContactsPage() { 
-    const [ contacts, setContacts ] = useState([]);
+export default function ViewContactsPage() {
+    const [allContacts, setAllContacts] = useState<Contact[]>([]);
+    const [contacts, setContacts] = useState<Contact[]>([]);
+    const [search, setSearch] = useState("");
+    const [sortAsc, setSortAsc] = useState(true);
 
     useEffect(() => {
         const storedData = localStorage.getItem("contacts");
-        setContacts( storedData ? JSON.parse( storedData ) : []);
-    }, [])
+        const parsed = storedData ? JSON.parse(storedData) : [];
+        setAllContacts(parsed);
+    }, []);
+
+    useEffect(() => {
+        let filtered = allContacts;
+
+        // Filter
+        if (search.trim() !== "") {
+            filtered = searchContact(search)
+        }
+
+        // Sort
+        const sorted = sortAsc
+            ? filtered.toSorted((a, b) => a.firstName.localeCompare(b.firstName))
+            : filtered.toSorted((a, b) => b.firstName.localeCompare(a.firstName));
+
+        setContacts(sorted);
+    }, [allContacts, search, sortAsc]);
 
     return (
         <div className="flex flex-col items-center p-6 gap-6">
-
             <p className="text-2xl text-bold sticky top-0 bg-black z-10 py-4 w-full text-center">
                 Saved Contacts
             </p>
-            { contacts.length === 0 ? (
+            <div className="flex flex-col w-full max-w-4xl gap-6">
+                <div className="flex gap-2">
+                    <input 
+                        name="search"
+                        placeholder="Search for contact" 
+                        value={ search }
+                        onChange={ (e) => setSearch(e.target.value) }
+                        className="w-full py-2 px-4 border rounded-full"
+                    />
+                    <button
+                    onClick={() => setSortAsc(!sortAsc)}
+                    className="">
+                        {sortAsc ? <SortDescIcon strokeWidth={1.25}/>: <SortAscIcon strokeWidth={1.25}/>}
+                    </button>
+                </div>
+
+                { contacts.length === 0 ? (
                 <div className="mt-10 text-gray-700 text-lg italic">
                     No contacts saved.
                 </div>
-            ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-4xl">
-                    {contacts.map((contact: Contact) => (
-                        contact.firstName === "" 
-                        ? <div className="hidden" key={contact.id}/>
-                        : <ContactCard key={contact.id} contact={contact}/>
-                    ))}
-                </div>
-            )
-            } 
-            
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
+                        {contacts.map((contact: Contact) => (
+                            contact.firstName === "" 
+                            ? <div className="hidden" key={contact.id}/>
+                            : <ContactCard key={contact.id} contact={contact}/>
+                        ))}
+                    </div>
+                )} 
+            </div>
         </div>
     );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,15 @@ import { Contact } from "@/model/Contact";
 export const deleteContact = (id: string) => {
     const storedData = localStorage.getItem("contacts");
     const contacts: Contact[] = storedData ? JSON.parse( storedData ) : [];
-    const filtered = contacts.filter((contact) => contact.id !== id)
+    const filtered = contacts.filter((contact) => contact.id !== id);
     localStorage.setItem("contacts", JSON.stringify(filtered));
 } 
+
+export const searchContact = (search: string) => {
+    const storedData = localStorage.getItem("contacts");
+    const contacts: Contact[] = storedData ? JSON.parse( storedData ) : [];
+    const searchText = search.toLowerCase();
+    const selected = contacts.filter((contact) => contact.firstName.toLowerCase().includes(searchText) || contact.lastName.toLowerCase().includes(searchText) || contact.phone.toLowerCase().includes(searchText) || contact.email.toLowerCase().includes(searchText) || contact.address.toLowerCase().includes(searchText));
+    return selected;
+}
+

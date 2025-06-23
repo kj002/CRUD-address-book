87463f3fbf7113adbc447b9a8c7acaee7338c0d9
Khadija Hafiz <khaf0001@student.monash.edu>
2025-06-24 03:35:18 +0800

    Refactor Home component to improve heading structure and enhance layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     <div className="flex justify-center pt-30 h-screen">
       <div className="flex flex-col gap-14 items-center w-full max-w-md">
         <div className="flex flex-col items-center text-center">
-          <p className="text-4xl">
+          <h2 className="text-4xl">
             Welcome to 
-          </p>
-          <p className="text-6xl text-bold mt-2 mb-10">
+          </h2>
+          <h1 className="text-6xl mt-2 mb-10 t-heading">
             PocketPal!
-          </p>
+          </h1>
           <p className="text-justify md:text-center">
             PocketPal is your cozy digital address book — a simple, elegant way to save, search, and organize the people who matter most. Add favorite contacts, mark them with a star, and keep your connections just a tap away.
           </p>
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="flex flex-col gap-6">
           <Link
           href={ "add-contact" }
-          className="text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
+          className="flex justify-center items-center h-12 w-64 text-center p-2 rounded-md border-1 hover:bg-gray-800 cursor-pointer">
             Add a New Contact
           </Link>
           <Link
@@ -28,7 +28,6 @@ export default function Home() {
             View Contacts
           </Link>
         </div>
-        
       </div>
     </div>
   );
